0b1cdd986ba1947409087db372775165de31bfb1
Ethan Mcintyre <mcintyea@mail.uc.edu>
2024-09-19 13:51:05 -0400

    Completed

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 22;
+        int intOperandB = 15;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        double doubleOperandA =22.22;
+        double doubleOperandB = 3.15;
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleQuotient = 0;
+        double doubleModulo = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using two ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product using two ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo of " + intOperandA + " and " + intOperandB + " is " + doubleModulo);
+
+
+        double lunchCost = 15.50;
+        int kidsinFamily = 5;
+        boolean isitraining = true;
+        double pricePerGallon = 3.00;
+        int FavoriteNumber = 8;
+        double ShoeSize = 12;
+        String birthmonth = "March";
+        String FullName = "Ethan Mcintyre";
+
+    }
+}
